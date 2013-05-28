a5355a5fc4d550446acf6e5ff723038045b4a31b
Sylvain Beorchia <sylvain.beorchia@makina-corpus.com>
2013-05-28 18:13:12 +0200

    Forms adds

--- coop_local/models/__init__.py
+++ coop_local/models/__init__.py
@@ -7,6 +7,7 @@ import sys
 from coop.utils.loading import load_models
 from coop import base_models
 from coop_local.models import local_models
+from ionyweb_plugins.page_coop_blog.models import CoopEntry
 
 main_module = sys.modules[__name__]
 
@@ -26,6 +27,7 @@ if "coop_tag" in settings.INSTALLED_APPS:
             help_text="Une liste de tags avec des virgules")
     t.contribute_to_class(Organization, "tags")
     t.contribute_to_class(Person, "tags")
+    t.contribute_to_class(CoopEntry, "tags")
     if "coop_cms" in settings.INSTALLED_APPS:
         t.contribute_to_class(Article, "tags")
     if "coop.exchange" in settings.INSTALLED_APPS:
