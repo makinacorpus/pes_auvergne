8f6a1fe105b7e381e6e82cf0d540652a221138ed
Sylvain Beorchia <sylvain.beorchia@makina-corpus.com>
2013-07-01 13:51:26 +0200

    Counter for textarea limited

--- pes_auvergne_ionyweb/themes/pes_auvergne/default/js/forms_utils.js
+++ pes_auvergne_ionyweb/themes/pes_auvergne/default/js/forms_utils.js
@@ -1,16 +1,25 @@
-    function cloneMore(selector, type) {
-        var newElement = $(selector).clone(true);
-        var total = $('#id_' + type + '-TOTAL_FORMS').val();
-        newElement.find(':input').each(function() {
-            var name = $(this).attr('name').replace('-' + (total-1) + '-','-' + total + '-');
-            var id = 'id_' + name;
-            $(this).attr({'name': name, 'id': id}).val('').removeAttr('checked');
-        });
-        newElement.find('label').each(function() {
-            var newFor = $(this).attr('for').replace('-' + (total-1) + '-','-' + total + '-');
-            $(this).attr('for', newFor);
-        });
-        total++;
-        $('#id_' + type + '-TOTAL_FORMS').val(total);
-        $(selector).after(newElement);
+function cloneMore(selector, type) {
+    var newElement = $(selector).clone(true);
+    var total = $('#id_' + type + '-TOTAL_FORMS').val();
+    newElement.find(':input').each(function() {
+        var name = $(this).attr('name').replace('-' + (total-1) + '-','-' + total + '-');
+        var id = 'id_' + name;
+        $(this).attr({'name': name, 'id': id}).val('').removeAttr('checked');
+    });
+    newElement.find('label').each(function() {
+        var newFor = $(this).attr('for').replace('-' + (total-1) + '-','-' + total + '-');
+        $(this).attr('for', newFor);
+    });
+    total++;
+    $('#id_' + type + '-TOTAL_FORMS').val(total);
+    $(selector).after(newElement);
+}
+    
+    
+function textCounter( field, maxLimit) {
+    if ( field.value.length > maxLimit )
+    {
+        field.value = field.value.substring( 0, maxLimit );
+        return false;
     }
+}
