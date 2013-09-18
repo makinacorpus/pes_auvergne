7bd804cdf15767d6efe277aa4a18684cc54d9f2f
Antoine Cezar <antoine.cezar@makina-corpus.com>
2013-09-18 14:44:43 +0200

    Add db dump and restore scripts

---
+++ pg_dump.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+[ -f .env ] && source .env
+
+if [ -z "$DB_USER" ]; then
+    echo "ERROR: DB_USER is not defined"
+    exit 1
+fi
+if [ -z "$DB_NAME" ]; then
+    echo "ERROR: DB_NAME is not defined"
+    exit 1
+fi
+if [ -z "$1" ]; then
+    OUTPUT="db_`date +%F_%H-%M-%S`.sql.gz"
+else
+    OUTPUT="$1"
+fi
+
+pg_dump -U "$DB_USER" -h localhost "$DB_NAME" \
+    | sed "s/$DB_USER/{{DB_USER}}/g" | gzip > "$OUTPUT"
---
+++ pg_restore.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -e
+
+[ -f .env ] && source .env
+
+if [ -z "$DB_USER" ]; then
+    echo "ERROR: DB_USER is not defined"
+    exit 1
+fi
+if [ -z "$DB_NAME" ]; then
+    echo "ERROR: DB_NAME is not defined"
+    exit 1
+fi
+if [ -z "$DOMAIN" ]; then
+    echo "ERROR: DOMAIN is not defined"
+    exit 1
+fi
+if [ -z "$1" ]; then
+    echo "ERROR: No dump file provided"
+    exit 1
+fi
+
+PSQL="sudo -u postgres psql -d $DB_NAME"
+
+$PSQL -c "DROP SCHEMA public CASCADE;"
+$PSQL -c "CREATE SCHEMA public AUTHORIZATION $DB_USER;"
+
+gzip -cd $1 \
+    | sed "s/{{DB_USER}}/$DB_USER/g" \
+    | $PSQL
+
+$PSQL -c "UPDATE django_site SET name='localhost', domain='$DOMAIN' WHERE id=1;"
+$PSQL -c "UPDATE multisite_alias SET domain='$DOMAIN' WHERE id=1;"
