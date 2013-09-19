7c6c097a40674ea164f16fe6422ad89504a8ce1b
Antoine Cezar <antoine.cezar@makina-corpus.com>
2013-09-19 15:02:38 +0200

    Add db and user recreation guards

--- vagrant_bootstrap.sh
+++ vagrant_bootstrap.sh
@@ -14,6 +14,11 @@ fi
 if [ -z "$DB_NAME" ]; then
     DB_NAME=coop_mes
 fi
+if [ -z "$DOMAIN" ]; then
+    DOMAIN=localhost
+fi
+
+PSQL="sudo -u postgres psql"
 
 ### FUNCTIONS ###
 echo_red() {
@@ -48,24 +53,37 @@ configure_postgres() {
     sudo service postgresql restart
 }
 
+db_user_exists() {
+    $PSQL -tAc "SELECT 'ok' FROM pg_roles WHERE rolname='$1'" \
+        | grep 'ok' &> /dev/null
+}
+
 create_db_user() {
-    echo_red "##################"
-    echo_red "# Create DB User #"
-    echo_red "##################"
-    sudo -u postgres psql -c "CREATE USER ${DB_USER} PASSWORD '${DB_PASS}';"
+    if ! db_user_exists "${DB_USER}"; then
+        echo_red "##################"
+        echo_red "# Create DB User #"
+        echo_red "##################"
+        $PSQL -c "CREATE USER ${DB_USER} PASSWORD '${DB_PASS}';"
+    fi
+}
+
+db_exists() {
+    $PSQL -tAl | grep "^$1" &> /dev/null
 }
 
 create_db() {
-    echo_red "#############"
-    echo_red "# Create DB #"
-    echo_red "#############"
-    sudo -u postgres psql -c "CREATE DATABASE ${DB_NAME} OWNER ${DB_USER} ENCODING 'UTF8' LC_COLLATE 'C' LC_CTYPE 'C' TEMPLATE template0;"
+    if ! db_exists "${DB_NAME}"; then
+        echo_red "#############"
+        echo_red "# Create DB #"
+        echo_red "#############"
+        $PSQL -c "CREATE DATABASE ${DB_NAME} OWNER ${DB_USER} ENCODING 'UTF8' LC_COLLATE 'C' LC_CTYPE 'C' TEMPLATE template0;"
 
-    # Add postgis
-    sudo -u postgres psql -d ${DB_NAME} -f /usr/share/postgresql/9.1/contrib/postgis-1.5/postgis.sql
-    sudo -u postgres psql -d ${DB_NAME} -f /usr/share/postgresql/9.1/contrib/postgis-1.5/spatial_ref_sys.sql
-    sudo -u postgres psql -d ${DB_NAME} -c "ALTER TABLE geometry_columns OWNER TO ${DB_USER};"
-    sudo -u postgres psql -d ${DB_NAME} -c "ALTER TABLE spatial_ref_sys OWNER TO ${DB_USER};"
+        # Add postgis
+        $PSQL -d ${DB_NAME} -f /usr/share/postgresql/9.1/contrib/postgis-1.5/postgis.sql
+        $PSQL -d ${DB_NAME} -f /usr/share/postgresql/9.1/contrib/postgis-1.5/spatial_ref_sys.sql
+        $PSQL -d ${DB_NAME} -c "ALTER TABLE geometry_columns OWNER TO ${DB_USER};"
+        $PSQL -d ${DB_NAME} -c "ALTER TABLE spatial_ref_sys OWNER TO ${DB_USER};"
+    fi
 }
 
 install_app() {
@@ -76,13 +94,13 @@ install_app() {
 	make install
 
     ### ??? ###
-    sudo -u postgres psql -d ${DB_NAME} -c "UPDATE django_site SET name='localhost', domain='localhost:8000' WHERE id=1;"
-    sudo -u postgres psql -d ${DB_NAME} -c "UPDATE multisite_alias SET domain='localhost:8000' WHERE id=1;"
+    $PSQL -d ${DB_NAME} -c "UPDATE django_site SET name='localhost', domain='${DOMAIN}' WHERE id=1;"
+    $PSQL -d ${DB_NAME} -c "UPDATE multisite_alias SET domain='${DOMAIN}' WHERE id=1;"
 }
 
 ### MAIN ###
-install_system_dependencies
-configure_postgres
+#install_system_dependencies
+#configure_postgres
 create_db_user
 create_db
-install_app
+#install_app
