1b13e0c063ef7a41d5a938d39aedb88857b05696
Sylvain Beorchia <sylvain.beorchia@makina-corpus.com>
2013-10-08 17:06:25 +0200

    Evaluation form

--- coop_local/models/local_models.py
+++ coop_local/models/local_models.py
@@ -2,6 +2,7 @@
 from django.db import models
 from extended_choices import Choices
 from coop.org.models import BaseOrganization
+from django.utils.translation import ugettext_lazy as _
 
 SECTEURS_FSE = Choices(
     ('TOUS',        0,  'Tous secteurs d’activité'),
@@ -15,6 +16,60 @@ SECTEURS_FSE = Choices(
     ('AUTRE',       8,  'Autres activités')
 )
 
+EVALUATE_ANSWERS = Choices(
+    ('NONE', 1, _(u'None')),
+    ('INTERESTED', 2, _(u'No action but interest')),
+    ('REGULAR', 3, _('Regular actions')),
+    ('SPECIALIST', 4, _('Specialist')),
+)
+    
+class EvaluationQuestionTheme(models.Model):
+    label = models.CharField(blank=True, max_length=1024)
+    cssclass = models.CharField(blank=True, max_length=50)
+    class Meta:
+        verbose_name = _(u'evaluate theme')
+        verbose_name_plural = _(u'evaluate theme')
+        ordering = ['label']
+        app_label = 'coop_local'
+
+    def __unicode__(self):
+        return self.label
+
+class Evaluation(models.Model):
+    title = models.CharField(max_length=200, blank=True, null=True)
+    class Meta:
+        verbose_name = _(u'evaluate')
+        verbose_name_plural = _(u'evaluate')
+        app_label = 'coop_local'
+
+class EvaluationQuestion(models.Model):
+    label = models.CharField(blank=True, max_length=512)
+    question = models.CharField(max_length=512)
+    theme = models.ForeignKey(EvaluationQuestionTheme,blank=True, null=True)
+
+    class Meta:
+        verbose_name = _(u'evaluation question')
+        verbose_name_plural = _(u'evaluation question')
+        app_label = 'coop_local'
+
+    def __unicode__(self):
+        return self.question
+
+
+class EvaluationAnswer(models.Model):
+    evaluation = models.ForeignKey(Evaluation)
+    question = models.ForeignKey(EvaluationQuestion)
+    answer = models.SmallIntegerField(verbose_name=_(u'answer'),choices=EVALUATE_ANSWERS, blank=True, null=True)        
+
+    class Meta:
+        verbose_name = _(u'evaluation answer')
+        verbose_name_plural = _(u'evaluation answer')
+        app_label = 'coop_local'
+
+    def __unicode__(self):
+        return self.answer
+
+
 class Organization(BaseOrganization):
     siret = models.CharField('Numero SIRET', blank=True, null=True, max_length=20)
     naf = models.CharField('Code d’activité NAF', blank=True, null=True, max_length=10)
@@ -25,6 +80,8 @@ class Organization(BaseOrganization):
     crowdfunding = models.URLField(u'crowdfunding', blank=True, null=True)
     description2 = models.TextField(u'en savoir plus', blank=True, null=True)
 
+    evaluation = models.ForeignKey('coop_local.Evaluation', verbose_name=_(u'answer'), blank=True, null=True)
+    
     class Meta:
         verbose_name = 'Initiative'
         verbose_name_plural = 'Initiatives'
